47d8e66e4e78ac7a22a5392a1799221c579f3873
Naren Ranjit <narendran.ranjit@gmail.com>
2018-05-29 15:00:31 -0700

    cleanup run api get

--- api/run/index.js
+++ api/run/index.js
@@ -1,53 +1,6 @@
-
-// function verifyAccessForRun(runid, userid) {
-//     // request.get()
-// }
-
-// api.post('*', ()=> {
-//     //directly pipe
-// });
-
-// //Do this as a middleware?
-// api.patch(/run/:runid/, (req, res)=> {
-//     verifyAccessForRun(runid, userid).then(()=> {
-//         res.pipe()
-//     }, ()=> {
-//         res.end(401);
-//     });
-// });
-// //Do this as a middleware?
-// api.get(/run/:runfilter/, (req, res)=> {
-//     if (runfilter.indexOf(';') !== -1) {
-//         //inject scope.user and call it a day;
-//     }
-    
-//     verifyAccessForRun(runid, userid).then(()=> {
-//         res.pipe()
-//     }, ()=> {
-//         res.end(401);
-//     });
-// });
-// //Do this as a middleware?
-// api.get(/run/:runid/va, (req, res)=> {
-//     verifyAccessForRun(runid, userid).then(()=> {
-//         res.pipe()
-//     }, ()=> {
-//         res.end(401);
-//     });
-// });
-
-
 const serverRoot = 'https://api.forio.com/v2';
 let request = require('request');
 request = request.defaults({jar: true});
-
-const JSONStream = require('JSONStream');
-const streamFilter = require('stream-filter');
-
-// module.exports = function defaultAPIPassThroughMiddleware(req, res) {
-//     const apiURL = `${serverRoot}/${req.url}`;
-//     req.pipe(request(apiURL)).pipe(res);
-// };
 
 const express = require('express');
 const runRouter = express.Router();
@@ -64,17 +17,14 @@ runRouter.get('/run/:account/:project/:runfilter', (req, res, next)=> {
 
     const validStatusCodes = [200, 201];
     request({
-        method: 'GET',
+        method: req.method,
         url: apiURL,
         gzip: false,
         json: true,
         headers: headers
     }, (err, response, body)=> {
-        // if (err) {
-        //     res.status(response.status).send.res
-        // }
         res.status(response.statusCode);
-        if (validStatusCodes.indexOf(response.statusCode) !== -1) {
+        if (err || validStatusCodes.indexOf(response.statusCode) !== -1) {
             return res.send(body);
         }
         if (Array.isArray(body)) { //multirun filter
@@ -84,7 +34,6 @@ runRouter.get('/run/:account/:project/:runfilter', (req, res, next)=> {
         if (isValidRun(body, req.user)) {
             return res.send(body);
         } 
-        console.log(err, response, body); 
         res.send(body);
     });
 });
